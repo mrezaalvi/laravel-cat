1e746949769879ecde62d51f90a1aa817ba1a3ab
Muhammad Reza Alvi <mrezaalvi@gmail.com>
2022-01-19 02:37:00 +0700

    update package.json and style with tailwindcss

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="w-screen h-full bg-gray flex justify-center items-center">
+    <h1 class="text-5xl text-yellow-500">
+        Under Construction
+    </h1>
+</body>
+</html>
