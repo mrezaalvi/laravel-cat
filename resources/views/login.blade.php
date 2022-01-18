4ef0d28a9ac72e559a6c9cbd4149c1bd7110cb48
Muhammad Reza Alvi <mrezaalvi@gmail.com>
2022-01-19 02:52:37 +0700

    fixed style

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
-    <link href="{{ asset('css/app.css') }}" rel="preload">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="w-screen h-full bg-gray flex justify-center items-center">
     <h1 class="text-5xl text-yellow-500">
